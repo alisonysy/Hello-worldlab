d7a6c517cb04eca1ced511d394d0e54c66fb96aa
alisonysy <qwen735@aucklanduni.ac.nz>
2016-09-30 14:34:59 +1300

    change the code again and add new cubes

--- Assets/Sketch.cs
+++ Assets/Sketch.cs
@@ -23,7 +23,24 @@ public class Sketch : MonoBehaviour {
         //----------------------
         //YOU WILL NEED TO DECLARE SOME VARIABLES HERE SIMILAR TO THE CREATIVE CODING TUTORIAL
 
+            int totalCubes = 20;
 
+            float totalDistance = 2.9f;
+
+            for (int i = 0; i < totalCubes; i++)
+            {
+                float perc = i / (float)totalCubes;
+
+                float sin = Mathf.Sin(perc * Mathf.PI / 2);
+
+                float x = 1.8f + sin * totalDistance;
+                float y = 5.0f;
+                float z = 0.0f;
+
+                var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+                newCube.GetComponent<CubeScript>().SetSize(.45f * (1.0f - perc));
+                newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc * 4.0f;
+        }   
 
         //----------------------
 
